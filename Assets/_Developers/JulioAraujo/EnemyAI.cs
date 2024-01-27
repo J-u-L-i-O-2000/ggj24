b5d6a3297128a899c5377b598ec78526a58bdf53
JulioAraujo <jsagamingofficial@gmail.com>
2024-01-27 00:03:17 +0000

    Added functionality to rise and fall based on if its being clicked
    kachow

--- Assets/_Developers/JulioAraujo/EnemyAI.cs
+++ Assets/_Developers/JulioAraujo/EnemyAI.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private Renderer enemyRenderer;
+    private Rigidbody2D rb;
     [SerializeField] private bool spawnLeft;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float floatSpeed;
     [SerializeField] private float health;
     [SerializeField] private bool reachedCastle = false;
+    [SerializeField] private bool isClicked = false;
     private int direction;
 
 
@@ -16,6 +19,8 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        enemyRenderer = GetComponent<Renderer>();
         if (spawnLeft)
         {
             direction = 1;
@@ -23,7 +28,9 @@ public class EnemyAI : MonoBehaviour
         else
         {
             direction = -1;
-        }     
+        }
+
+        
     }
 
     // Update is called once per frame
@@ -32,6 +39,21 @@ public class EnemyAI : MonoBehaviour
         transform.position += direction*Vector3.right * Time.deltaTime * walkSpeed;
 
 
+
+        if (isClicked)
+        {
+            rb.gravityScale = 0f;
+            transform.position += Vector3.up * Time.deltaTime * floatSpeed;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+
+        if(isClicked && !enemyRenderer.isVisible)
+        {
+            Debug.Log("Im dead bro");
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
